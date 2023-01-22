9b4d9b50e449078c82fffa73190913a3e07b7253
semnil5202 <semnil5202@gamil.com>
2023-01-22 12:54:28 +0900

    git push test
    macbook air

--- js/view/NumberOfUserView.js
+++ js/view/NumberOfUserView.js
@@ -2,10 +2,10 @@ import { eventListener } from '../utils/handler.js';
 
 export default class NumberOfUserView {
   numbers = new Map([
-    ['firstNumber', document.querySelector('.number-item-container button:nth-child(1)')],
-    ['secondNumber', document.querySelector('.number-item-container button:nth-child(2)')],
-    ['thirdNumber', document.querySelector('.number-item-container button:nth-child(3)')],
-    ['forthNumber', document.querySelector('.number-item-container button:nth-child(4)')],
+    ['firstNumber', document.querySelector('.number-item-container div:nth-child(1)')],
+    ['secondNumber', document.querySelector('.number-item-container div:nth-child(2)')],
+    ['thirdNumber', document.querySelector('.number-item-container div:nth-child(3)')],
+    ['forthNumber', document.querySelector('.number-item-container div:nth-child(4)')],
   ]);
 
   init() {
@@ -15,7 +15,6 @@ export default class NumberOfUserView {
   }
 
   conditionFunc(event) {
-    if (event.target.className === 'number-item') event.target.className = 'number-item-active';
-    else event.target.className = 'number-item';
+    console.dir(event.target.checked);
   }
 }
