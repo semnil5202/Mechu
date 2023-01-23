ab0e770e76340006bdfeea81352fe11cd0e46ac2
semnil5202 <semnil5202@gmail.com>
2023-01-23 20:01:47 +0900

    feat: 사용자가 입력한 값을 가공하는 기능 추가
    todo :
    뷰로부터 입력받은 값 도메인으로 넘겨서 데이터 처리
    처리된 데이터 결과값으로 메뉴 추천 뷰 생성

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import AppController from './controller/AppController.js';
 
 export default class App {
   start() {
-    new AppController().start();
+    new AppController();
   }
 }
 
--- js/controller/AppController.js
+++ js/controller/AppController.js
@@ -1,7 +1,32 @@
+import kindOfCategoryView from '../view/kindOfCategoryView.js';
 import NumberOfUserView from '../view/NumberOfUserView.js';
+import { eventListener } from '../utils/handler.js';
 
 export default class AppController {
-  start() {
-    new NumberOfUserView().init();
+  confirmButton;
+  numberOfUserView;
+  kindOfCategoryView;
+
+  constructor() {
+    this.confirmButton = document
+      .querySelector('.confirm-button button')
+      .addEventListener('click', this.submitForm);
+    this.numberOfUserView = new NumberOfUserView(); // DI 필요
+    this.kindOfCategoryView = new kindOfCategoryView();
+  }
+
+  submitForm(event) {
+    const number = document.querySelector('.number-item input:checked + label');
+    const kindOf = document.querySelector('.kind-of-item input:checked + label');
+    const category = document.querySelector('.category-item input:checked + label');
+    const notEatList = document.querySelector('.not-eat-item-container');
+    const notEats = new Map();
+
+    for (let i = 0; i < notEatList.childElementCount; i++) {
+      notEats.set(
+        `${i + 1}`,
+        document.querySelector(`.not-eat-item-container div:nth-child(${i + 1}) input`).value,
+      );
+    }
   }
 }
