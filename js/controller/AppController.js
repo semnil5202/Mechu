9efe0e1ad952c0a174900e454de1c34a679533de
semnil5202 <semnil5202@gmail.com>
2023-01-24 01:03:12 +0900

    refactor: addEventListener 콜백함수 방식 변경으로 인한 수정
    추가로 AppService 클래스에 사용자의 입력값을 바탕으로 만들어진 데이터를 전달한다.

--- js/controller/AppController.js
+++ js/controller/AppController.js
@@ -1,32 +1,44 @@
 import kindOfCategoryView from '../view/kindOfCategoryView.js';
 import NumberOfUserView from '../view/NumberOfUserView.js';
-import { eventListener } from '../utils/handler.js';
+import AppService from '../service/appService.js';
 
 export default class AppController {
   confirmButton;
   numberOfUserView;
   kindOfCategoryView;
+  appService;
 
   constructor() {
     this.confirmButton = document
-      .querySelector('.confirm-button button')
-      .addEventListener('click', this.submitForm);
+      .querySelector('.confirm-form button')
+      .addEventListener('click', (event) => this.submitForm(event));
     this.numberOfUserView = new NumberOfUserView(); // DI 필요
     this.kindOfCategoryView = new kindOfCategoryView();
+    this.appService = new AppService();
   }
 
   submitForm(event) {
-    const number = document.querySelector('.number-item input:checked + label');
-    const kindOf = document.querySelector('.kind-of-item input:checked + label');
-    const category = document.querySelector('.category-item input:checked + label');
-    const notEatList = document.querySelector('.not-eat-item-container');
-    const notEats = new Map();
+    event.preventDefault();
 
-    for (let i = 0; i < notEatList.childElementCount; i++) {
-      notEats.set(
-        `${i + 1}`,
-        document.querySelector(`.not-eat-item-container div:nth-child(${i + 1}) input`).value,
-      );
+    const dataSet = {
+      number: document.querySelector('.number-item input:checked + label'),
+      kindOf: document.querySelector('.kind-of-item input:checked + label'),
+      category: document.querySelector('.category-item input:checked + label'),
+      notEatList: document.querySelector('.not-eat-item-container'),
+      notEats: new Map(),
+    };
+
+    for (let i = 0; i < dataSet.notEatList.childElementCount; i++) {
+      this.recordNotEatList(dataSet, i);
     }
+
+    this.appService.init(dataSet);
+  }
+
+  recordNotEatList(dataSet, i) {
+    dataSet.notEats.set(
+      `${i + 1}`,
+      document.querySelector(`.not-eat-item-container div:nth-child(${i + 1}) input`).value,
+    );
   }
 }
